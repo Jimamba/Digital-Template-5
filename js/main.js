eeebcac55a74b01bc28fb280e2d54834b0a45d2c
Jimamba <jimstanley@jims-mbp.home>
2015-03-05 02:10:54 -0500

    Final Version

--- js/main.js
+++ js/main.js
@@ -13,32 +13,60 @@ window.onload = function() {
     
     "use strict";
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, render: render } );
     
     function preload() {
         // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+       game.load.image('bomb', 'assets/Background-timer.png');
+       game.load.image('win','assets/win.png');
+       game.load.image('fail','assets/fail.png');
+       //game.load.audio('badaboom','assets/WTF BOOM.mp3');
+       game.load.audio('badaboom','assets/Bomb-SoundBible.com-891110113.mp3');
+       game.load.audio('music','assets/lightsaber soundtrack.mp3');
+       
     }
     
-    var bouncy;
+    var red;
+    var yellow;
+    var blue;
+    var green;
+    var effect;
+    var music;
+    
     
     function create() {
         // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
+        
+        effect = game.add.audio('badaboom',1,true);
+        music = game.add.audio('music');
+        music.play();
+        game.add.sprite(0,0,'bomb');
+      	game.physics.startSystem(Phaser.Physics.ARCADE);
+     	
         // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
+       
+       
+        
         
         // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
+      	
         // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
+       
+       
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
+        
+        var text = game.add.text(400, game.world.height - 40, 'Press q,w,e,r to try and stop the bomb', { fontSize: '8px', fill: '#000' });
         text.anchor.setTo( 0.5, 0.0 );
+        
+        
+        red = game.input.keyboard.addKey(Phaser.Keyboard.Q);
+        yellow = game.input.keyboard.addKey(Phaser.Keyboard.W);
+        blue = game.input.keyboard.addKey(Phaser.Keyboard.E);
+        green = game.input.keyboard.addKey(Phaser.Keyboard.R);
+        
+        game.time.events.add(Phaser.Timer.SECOND * 30, death, this);
     }
     
     function update() {
@@ -47,6 +75,41 @@ window.onload = function() {
         // in X or Y.
         // This function returns the rotation angle that makes it visually match its
         // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
+       if(red.isDown)
+       {
+       death();
+       } 
+       else if(yellow.isDown)
+       {
+       win();
+       }
+       else if(blue.isDown)
+       {
+       death();
+       }
+       else if(green.isDown)
+       {
+       death();
+       }
+       
+       
+        //end of update function
     }
+    function render() 
+    {
+	game.debug.text("Time until explosion: " + game.time.events.duration, 255, 300);
+
+	}
+	function death()
+	{
+	effect.play('',0,1,false);
+	game.add.sprite(0,0,'fail');
+	game.paused = true;
+	}
+	function win()
+	{
+	game.add.sprite(0,0,'win');
+	game.paused = true;
+	}
+	//end of everything
 };
